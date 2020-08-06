8b31ea331c0b5bc41502c7844db896029a251960
Christy Tropila <ctropilalakebeverage@gmail.com>
2020-08-06 23:31:20 +0000

    Automatically backed up by Learn

--- tools/console.rb
+++ tools/console.rb
@@ -7,12 +7,14 @@ end
 student1=Student.new("Christy")
 student2=Student.new("Jade")
 
+
 instructor1=Instructor.new("Sylwia")
 instructor2=Instructor.new("Eric")
 
 boattest1=BoatingTest.new(student1, "boating test name", "pass", instructor1)
 boattest2=BoatingTest.new(student2, "boating test name", "fail", instructor2)
 
+student1.add_boating_test(self, "pass", instructor2)
 
 
 binding.pry
